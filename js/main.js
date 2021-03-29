f710f7102b7c5e26b952984e2bb5de778018ab08
Derek Foreman <derekforeman@gmail.com>
2021-03-29 10:21:40 -0400

    disable autocorrect, spelling etc in input.

--- js/main.js
+++ js/main.js
@@ -7,9 +7,7 @@ var wordTemplate;
 
 //object.addEventListener("click", myScript);
 //document.getElementById("demo").innerHTML = "Hello World";
-
-function StartGame()
-{
+function StartGame() {
   isGameRunning = true;
   GetWordToGuess();
 
@@ -17,16 +15,17 @@ function StartGame()
   console.log(isGameRunning);
 }
 
-function GetWordToGuess()
-{
+function GetWordToGuess() {
   wordToGuess =  words[Math.floor(Math.random() * words.length)];
   wordToGuessLen = wordToGuess.length;
   wordTemplate = GetShadowStringForWord(wordToGuess);
+  document.getElementById('correctLetters').innerText = wordTemplate;
+  //set letters in word
+		document.getElementById('lettersInWord').innerText = 'There are ' + wordToGuessLen + ' letters in your word.';
   console.log(wordToGuessLen);
 }
 
-function AddGuessedLetter(letter)
-{
+function AddGuessedLetter(letter) {
   if(guessedLetters.includes(letter))
   {
     alert("You already guessed that Letter, try again!");
@@ -41,8 +40,7 @@ function AddGuessedLetter(letter)
   guessedLetters.forEach(item => console.log(item));
 }
 
-function Guess()
-{
+function Guess() {
   console.log("Guess pressed");
   if(!isGameRunning) {
     console.log("Start Game to Guess!");
@@ -55,24 +53,33 @@ function Guess()
   document.getElementById('letter').value = '';
   //did letter match word?
   if(wordToGuess.includes(letter)) {
-     //let pos = FindLetterPositionsInWord(letter);
+  		let pos = FindLetterPositionsInWord(letter);
     //update letter match.
+				UpdateShadowStringForLetters(pos, letter);
   }
-
+  CheckWin();
 }
 
-function Quit()
-{
+function CheckWin(){
+	let guessedWord = document.getElementById('correctLetters').innerText;
+	if(guessedWord === wordToGuess){
+		alert(`You win!! \n It took you ${guessedLetters.length} guesses.`);
+		Quit();
+	}
+}
+
+function Quit() {
   console.log('quit');
   //clear variable values and update screen.
   wordToGuess = '';
   isGameRunning = false;
   guessedLetters = [];
-  document.getElementById('GuessedLetters').innerText = '';
+  document.getElementById('lettersInWord').innerText = '';
+		document.getElementById('GuessedLetters').innerText = '';
+		document.getElementById('correctLetters').innerText = '';
 }
 
-function FindLetterPositionsInWord(letter)
-{
+function FindLetterPositionsInWord(letter) {
   var indices = [];
   for(var i = 0; i < wordToGuess.length; i++) {
     if (wordToGuess[i] === letter) indices.push(i);
@@ -80,10 +87,30 @@ function FindLetterPositionsInWord(letter)
   return indices;
 }
 
-function GetShadowStringForWord(word)
-{
+
+function replaceAt(str, index, ch) {
+	return str.replace(/./g, (c, i) => i == index ? ch : c);
+}
+
+function UpdateShadowStringForLetters(pos, letter) {
+	var shadow = document.getElementById('correctLetters').innerText;
+
+	// var split = shadow.split('');
+	// //each letter is separated by a space
+	// //after the first position (0) we need to skip the spaces
+	for (var i = 0; i < pos.length; i++) {
+		if(i == 0){
+			shadow = replaceAt(shadow,pos[i], letter);
+		}
+		shadow = replaceAt(shadow,pos[i + 1], letter);
+	}
+
+	document.getElementById('correctLetters').innerText = shadow;
+}
+
+function GetShadowStringForWord(word) {
   var len = word.length;
-  var template = "_ ";
+  var template = "_";
   var templateString = "";
   for (var i = 0; i < len; i++)
   {
@@ -92,7 +119,6 @@ function GetShadowStringForWord(word)
   return templateString;
 }
 
-function UpdateShadow(wordTemplate)
-{
+function UpdateShadow(wordTemplate) {
   document.getElementById('GuessedLetters').innerText = wordTemplate;
 }
